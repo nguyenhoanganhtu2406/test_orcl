3164f011642dcd5fdf252743d17ca732a0ffa553
Nguyen Hoang Anh Tu <nguyenhoanganhtu2406@gmail.com>
2019-05-16 11:57:03 +0700

    Adjust db and script

---
+++ ASPCore_Final/ASPCore_Final/Models/Sanphamkho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASPCore_Final.Models
+{
+    public partial class Sanphamkho
+    {
+        public int MaspKho { get; set; }
+        public int Mahh { get; set; }
+        public string Kichco { get; set; }
+        public int? Soluong { get; set; }
+
+        public Hanghoa MahhNavigation { get; set; }
+    }
+}
